92d41aaf050006939de884a9fa3a3fb824bfdb71
gyan-cell <jha07944@gmail.com>
2024-04-12 23:50:19 +0530

    Finished hero page

--- components/custom/server/HeroPage.tsx
+++ components/custom/server/HeroPage.tsx
@@ -1,8 +1,22 @@
+import Image from 'next/image'
 import React from 'react'
+import hero from '@/public/nvim.png'
+import '@/styles/heropage.scss'
+import Link from 'next/link'
 
 const HeroPage = () => {
   return (
-    <div>HeroPage</div>
+    <div className='hero-page' >
+      <div className='image'>
+        <Image src={hero} alt='hero' objectFit='cover' layout='fill' objectPosition='center' />
+      </div>
+      <div className='overlay' >
+        <div className='button' >
+          <button className='bg-background' ><Link href={'/docs'} >Get Started</Link></button>
+          <button className='bg-background' ><Link href={'/install'} >Docs</Link></button>
+        </div>
+      </div>
+    </div>
   )
 }
 
