9df84cbe409bbf82a4772e4431c85c5a083f2525
gyan-cell <jha07944@gmail.com>
2024-04-14 21:58:49 +0530

    Same Changes

--- components/custom/server/HeroPage.tsx
+++ components/custom/server/HeroPage.tsx
@@ -8,12 +8,12 @@ const HeroPage = () => {
   return (
     <div className='hero-page' >
       <div className='image'>
-        <Image src={hero} alt='hero' objectFit='cover' layout='fill' objectPosition='center' />
+        <Image src={hero} alt='hero' objectFit='contain' layout='fill' objectPosition='center' />
       </div>
       <div className='overlay' >
         <div className='button' >
-          <button className='bg-background' ><Link href={'/docs'} >Get Started</Link></button>
-          <button className='bg-background' ><Link href={'/install'} >Docs</Link></button>
+          <button className='' ><Link href={'/docs'} >Get Started</Link></button>
+          <button className='' ><Link href={'/install'} >Install</Link></button>
         </div>
       </div>
     </div>
